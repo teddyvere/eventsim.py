8e99b7951980607daf597588f499e19496781c3b
goodoc <goodoc@goodocui-MacBookPro.local>
2023-12-01 15:28:47 +0900

    test dags

--- dags/S3_Silver.py
+++ dags/S3_Silver.py
@@ -12,14 +12,15 @@ from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 def get_json_from_s3(**context):
     conn_info = Variable.get("AWS_S3_CONN", deserialize_json=True)
     s3_key = f"eventsim/date_id={context['exec_date']}.json"
+    print(conn_info)
     # Creating Session with Boto3
-    s3_session = boto3.client(
+    s3_client = boto3.client(
         's3',
         aws_access_key_id=conn_info['AWS_ACCESS_KEY_ID'],
         aws_secret_access_key=conn_info['AWS_SECRET_ACCESS_KEY']
     )
     # Creating Object From the S3 Resource
-    obj = s3_session.get_object(Bucket='eventsim', 
+    obj = s3_client.get_object(Bucket='eventsim', 
                                 Key='eventsim/date_id=2023-12-01.json')
     
     # Reading Json from S3
