5823fecc6b6a32a968de49f27d5a7a653740d053
Sungguk Jang <sunggukjang@Sunggukui-MacBookPro.local>
2023-12-09 20:21:49 +0900

    bronze_to_silver_with_spark

---
+++ dags/s3_silver_spark.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+
+with DAG (
+    dag_id='bronze_to_silver',
+    schedule='@once',
+    start_date=datetime(2023, 12, 9),
+    catchup=False
+) as dag:
+    spark_submit = SparkSubmitOperator(
+        task_id='spark_submit',
+        application='supports/spark_operator.py'
+    )
---
+++ dags/supports/spark_operator.py
@@ -0,0 +1,29 @@
+from dateimte import datetime
+
+import pandas as pd
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession, functions as f
+
+
+conf = SparkConf()
+conf.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+conf.set("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.2")
+
+spark = SparkSession.builder.config(conf=conf).appName("s3_injection").getOrCreate()
+
+df = spark.read.format("csv")\
+          .option("header", "true")\
+          .option("inferSchema", "true")\
+          .csv("s3a://eventsim/eventsim/date_id=2023-12-04-test.csv")
+
+# contert timestamp to datetime
+df.withColumn("ts", f.to_timestamp(f.col("ts")))\
+  .withColumn("date_id", f.to_date(f.col("ts")))
+
+grouped_df = df.groupBy("date_id")
+
+def put_by_date_id(date_id, table):
+    key = f"s3a://eventsim/silver1/date_id={date_id}/test0.csv"
+    table.write.mode("overwrite").parquet(key)
+
+grouped_df.foreach(lambda x: put_by_date_id(x[0], x[1]))
